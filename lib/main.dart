10be91bb065d5eb9186376db8ba14439fc3f114f
rickyricko302 <rickyricko302@gmail.com>
2022-11-29 13:36:47 +0700

    Source

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+import 'Screens/Login.dart';
+
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Login(),
+    ));
+//Copy right by CodingID wajib subscribe Fauko 
